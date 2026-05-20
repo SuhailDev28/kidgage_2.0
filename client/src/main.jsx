7ef9912a7b5014f4f91038710a0566c06923e57a
SuhailDev28 <suhailiqbal28@gmail.com>
2026-05-20 13:25:40 +0300

    Fix PWA registration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,4 @@
+// client/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -5,14 +6,16 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import { PublicSettingsProvider } from "./context/PublicSettingsProvider.jsx";
 import { AppRouter } from "./app/router.jsx";
+import PwaUpdatePrompt from "./components/PwaUpdatePrompt.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ""}>
       <BrowserRouter>
         <PublicSettingsProvider>
           <AppRouter />
+          <PwaUpdatePrompt />
         </PublicSettingsProvider>
       </BrowserRouter>
     </GoogleOAuthProvider>
