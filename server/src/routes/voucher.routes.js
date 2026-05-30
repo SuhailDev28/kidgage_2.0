e6be2347adfef4778cc177304b1d75959f026aaf
SuhailDev28 <suhailiqbal28@gmail.com>
2026-05-30 18:00:07 +0300

    import in vochure route change

--- server/src/routes/voucher.routes.js
+++ server/src/routes/voucher.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Voucher from "../../models/Voucher.js";
+import Voucher from "../models/Voucher.js";
 import { requireAuth, requireSuperAdmin } from "../../middleware/auth.js";
 
 const router = express.Router();
